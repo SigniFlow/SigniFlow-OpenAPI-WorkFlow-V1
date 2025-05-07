ca71576ea818aa35faf8f33201474a725bbe435d
lukei@signiflow.com <lukei@signiflow.com>
2025-05-07 09:08:46 +0200

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjcxNzgx/spectral.js?branch_slug=main&token=312c49c2-5bcd-4bcb-a87f-8943437c56fb";
+export default { extends: ruleset };
